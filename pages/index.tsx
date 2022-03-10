0cceb3a63abfa43777c0d767743738fc352f4c64
Jitu Nayak <jitunayak@Jitus-MacBook-Pro.local>
2022-03-10 16:48:35 +0530

    initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,47 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import Highlight from "react-highlight";
+import { useEffect, useState } from "react";
 
 const Home: NextPage = () => {
+  const [intefaceContent, setInterfaceContent] = useState(`
+  interface IEmployee {
+    empCode: number;
+    empName: string;
+    salary: number; 
+    managerName: string; 
+}`);
+  const [sampleJsonContent, setSampleJsonContent] = useState("");
+
+  useEffect(() => {
+    let sampleJson = intefaceContent.trim().split("{")[1];
+    sampleJson = "{\n" + sampleJson;
+    sampleJson = sampleJson
+      .replace(/number+/g, "2")
+      .replace(/string+/g, "test")
+      .replace(/;+/g, ",")
+      .replace(/:+/g, ":")
+      .substring(2, sampleJson.length - 4);
+    // .substring(0, sampleJson.length - 5) + "}".trim();
+
+    let words = sampleJson.split(",");
+
+    let final;
+    for (let s of words) {
+      final =
+        final + `"${s.trim().split(":")[0]}":"${s.trim().split(":")[1]}",`;
+      console.log(`"${s.split(":")[1]}"`);
+    }
+    final = final?.replace(/undefined+/g, "");
+    final = final?.substring(0, final.length - 8);
+
+    final = `{${final}}`;
+
+    setSampleJsonContent(final);
+  }, [intefaceContent]);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -14,59 +52,42 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Convert Interfaces to Sample JSON Payload
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
+          Paste or write your interface below
+          <code className={styles.code}> text input</code>
         </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
 
+      <div>
+        <textarea
+          className={styles.editor}
+          value={intefaceContent}
+          onChange={(e) => setInterfaceContent(e.target.value)}
+        ></textarea>
+
+        <textarea
+          className={styles.editor}
+          value={sampleJsonContent}
+          onChange={(e) => setSampleJsonContent(e.target.value)}
+        ></textarea>
+      </div>
       <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
